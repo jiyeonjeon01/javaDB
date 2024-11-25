ccfa0b5a92f2e182fc5a498f94d1cf592cf52738
Jiyeon Jeon <jiyeonjeon01@naver.com>
2024-11-25 17:57:37 +0900

    [create] subjectMVCProject 중간 커밋

---
+++ subjectMVCProject/src/com/kh/subjectMVCProject/controller/Test.java
@@ -0,0 +1,15 @@
+package com.kh.subjectMVCProject.controller;
+
+import java.sql.Connection;
+
+public class Test {
+	public static void main(String[] args) {
+		Connection con = DBUtility.dbCon();
+		if(con != null) {
+			System.out.println("데이터 베이스 접속 성공");
+		}else {
+			System.out.println("데이터 베이스 접속 실패");
+		}
+	}
+
+}
