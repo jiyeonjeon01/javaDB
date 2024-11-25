7dcebb302312bb557353d1068bb0ed691c530682
Jiyeon Jeon <jiyeonjeon01@naver.com>
2024-11-25 17:58:16 +0900

    [update] studentTest 추가 수정

--- studentsTest/src/com/kh/java/controller/DBUtility.java
+++ studentsTest/src/com/kh/java/controller/DBUtility.java
@@ -1,29 +1,42 @@
 package com.kh.java.controller;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class DBUtility {
-	public static Connection dbCon() {
-
+	public static Connection dbCon()  {
 		Connection con = null;
-
-		// 1. jdbc driver load
-		// 2. connection
+		// db.properties 에서 id, pw 가져오기
+		String filePath = "C:\\Users\\PC\\javaDB\\studentsTest\\src\\db.properties";
+		Properties pt = new Properties(); 
 		try {
-		Class.forName("oracle.jdbc.driver.OracleDriver");
-		con = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521/xe", "hr", "hr");
+			pt.load(new FileReader(filePath));
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+		String id = pt.getProperty("id");
+		String pw = pt.getProperty("pw");
+		String url = pt.getProperty("url");
+		
+		// 1. jdbc driver load 2. connection
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(url, id, pw);
 		} catch (ClassNotFoundException e) {
 			System.out.println(e.toString());
 		} catch (SQLException e) {
 			System.out.println(e.toString());
 		}
-			return con;
-		}
+		return con;
+	}
 
 		public static void dbClose(Connection con, Statement stmt, ResultSet rs) {
 			if (con != null) {
