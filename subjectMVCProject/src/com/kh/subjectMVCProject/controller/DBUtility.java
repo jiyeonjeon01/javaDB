ad3eb1cb2bc5df94752d54149c9504036c33b5fa
Jiyeon Jeon <jiyeonjeon01@naver.com>
2024-11-27 15:44:34 +0900

    [update] DBUtility 업데이트

--- subjectMVCProject/src/com/kh/subjectMVCProject/controller/DBUtility.java
+++ subjectMVCProject/src/com/kh/subjectMVCProject/controller/DBUtility.java
@@ -12,9 +12,10 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DBUtility {
+
 	public static Connection dbCon()  {
 		Connection con = null;
-		// db.properties file 에서 id, pw 가져오기
+		// 1. db.properties file( id, pw, url setting)
 		String filePath = "C:\\Users\\PC\\javaDB\\subjectMVCProject\\src\\db.properties";
 		Properties pt = new Properties(); 
 		try {
@@ -27,7 +28,7 @@ public class DBUtility {
 		String url = pt.getProperty("url");
 		
 		// 2. jdbc driver load 
-		// 3. db connect
+		// 3. db connect 
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			con = DriverManager.getConnection(url, id, pw);
@@ -38,70 +39,79 @@ public class DBUtility {
 		}
 		return con;
 	}
+	//오버로딩 
+	public static void dbClose(Connection con, Statement stmt, ResultSet rs) {
+		if (con != null) {
+			try {
+				con.close();
 
-		public static void dbClose(Connection con, Statement stmt, ResultSet rs) {
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					System.out.println(e.toString());
-				}
+			} catch (SQLException e) {
+				System.out.println(e.toString());
 			}
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e) {
-					System.out.println(e.toString());
-				}
+		}
+		if (stmt != null) {
+			try {
+				stmt.close();
+
+			} catch (SQLException e) {
+				System.out.println(e.toString());
 			}
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					System.out.println(e.toString());
-				}
+		}
+		if (rs != null) {
+			try {
+				rs.close();
+
+			} catch (SQLException e) {
+				System.out.println(e.toString());
 			}
 		}
 
-		public static void dbClose(Connection con, Statement stmt) {
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					System.out.println(e.toString());
-				}
-			}
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e) {
-					System.out.println(e.toString());
-				}
+	}
+
+	public static void dbClose(Connection con, Statement stmt) {
+		if (con != null) {
+			try {
+				con.close();
+
+			} catch (SQLException e) {
+				System.out.println(e.toString());
 			}
 		}
-		
-		public static void dbClose(Connection con, Statement stmt, CallableStatement cstmt) {
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					System.out.println(e.toString());
-				}
+		if (stmt != null) {
+			try {
+				stmt.close();
+
+			} catch (SQLException e) {
+				System.out.println(e.toString());
 			}
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (SQLException e) {
-					System.out.println(e.toString());
-				}
+		}
+
+	}
+
+	public static void dbClose(Connection con, Statement stmt, CallableStatement cstmt) {
+		if (con != null) {
+			try {
+				con.close();
+
+			} catch (SQLException e) {
+				System.out.println(e.toString());
 			}
-			if (cstmt != null) {
-				try {
-					cstmt.close();
-				} catch (SQLException e) {
-					System.out.println(e.toString());
-				}
+		}
+		if (stmt != null) {
+			try {
+				stmt.close();
+
+			} catch (SQLException e) {
+				System.out.println(e.toString());
 			}
-		}	
+		}
+		if (cstmt != null) {
+			try {
+				cstmt.close();
+
+			} catch (SQLException e) {
+				System.out.println(e.toString());
+			}
+		}
+	}
 }
-
